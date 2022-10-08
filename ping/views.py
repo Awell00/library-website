46d9a2222a12b5c0583d243abe9402d31506c7b3
Awell <awellpro@gmail.com>
2022-10-08 21:04:28 +0200

    add sqlite

--- ping/views.py
+++ ping/views.py
@@ -1,8 +1,29 @@
 from django.http import HttpResponse
 from .models import ToDOLIst, Item
+import sqlite3  
+
+
+def ajouter_adherent():
+    nom=input("Nom du nouvel adhérent ?: ")
+    # prenom=input("Prénom du nouvel adhérent ?: ")
+    # adresse=input("Adresse: ")
+    # tel=input("Son tel: ")
+    res = cur.execute('INSERT INTO '+nom+' VALUES("test","test","test")')
+    con.commit()
+    test = cur.execute('SELECT * FROM '+nom)
+    test2 = test.fetchall()
+    return test2
+
+con = sqlite3.connect("bibliotheque.db")
+cur = con.cursor()
+res = cur.execute('SELECT * FROM h')
+con.commit()
+test = ajouter_adherent()
 
 def index(response, name):
+    
     ls = ToDOLIst.objects.get(name=name)
     item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1>" % (ls.name, item.name))
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (test, item.text))
 
+print(ajouter_adherent())
