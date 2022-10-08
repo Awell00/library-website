29b675cb53bc9e09186a6f5d7cbdc40eb29d9df5
Awell <67506066+Awell00@users.noreply.github.com>
2022-10-08 15:39:51 +0000

    add

--- ping/views.py
+++ ping/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from .models import ToDOLIst, Item
 
-def index(response, id):
-    ls = ToDOLIst.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>" % ls.name)
+def index(response, name):
+    ls = ToDOLIst.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1>" % (ls.name, item.name))
 
