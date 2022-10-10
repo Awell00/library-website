60d64e31ea08f4e0f9c3a480b15a0e047e04fe4a
Awell <67506066+Awell00@users.noreply.github.com>
2022-10-10 19:22:00 +0200

    Add files via upload

--- ping/views.py
+++ ping/views.py
@@ -1,29 +1,32 @@
+from readline import add_history
 from django.http import HttpResponse
-from .models import Test, Item
-import sqlite3  
+from .models import ToDOLIst, Item
+import sqlite3
 
 
-# def ajouter_adherent():
-#     nom=input("Nom du nouvel adhérent ?: ")
-#     # prenom=input("Prénom du nouvel adhérent ?: ")
-#     # adresse=input("Adresse: ")
-#     # tel=input("Son tel: ")
-#     res = cur.execute('INSERT INTO '+nom+' VALUES("test","test","test")')
-#     con.commit()
-#     test = cur.execute('SELECT * FROM '+nom)
-#     test2 = test.fetchall()
-#     return test2
+def ajouter_adherent():
+    nom=input("Nom du nouvel adhérent ?: ")
+    prenom=input("Prénom du nouvel adhérent ?: ")
+    adresse=input("Adresse: ")
+    tel=input("Son tel: ")
+    requete='INSERT INTO adherent(nomAdherent, prenomAdherent, adresse, telephone) VALUES ("'+nom+'","'+prenom+'", "'+adresse+'","'+tel+'")'
+    cur.execute(requete)
+    res3 = cur.execute("SELECT nomAdherent FROM adherent")
+    con.commit()
+    return res3.fetchall()
 
-# con = sqlite3.connect("bibliotheque.db")
-# cur = con.cursor()
-# res = cur.execute('SELECT * FROM h')
-# con.commit()
-# test = ajouter_adherent()
+con = sqlite3.connect("bibliotheque.db")
+cur = con.cursor()
+res = cur.execute('SELECT * FROM Test')
+con.commit()
+test = ajouter_adherent()
+
+# test = res.fetchall()
+
+
 
 def index(response, name):
-    
-    ls = Test.objects.get(name=name)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (ls.name, item.text))
+    # ls = ToDOLIst.objects.get(name=name)
+    # item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" % (test, name))
 
-# print(ajouter_adherent())
